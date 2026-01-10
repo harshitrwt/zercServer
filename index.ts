54db917267dbd2e83e7ab716a7087a2e4a38dd95
harshitrwt <harshit.rawat1804@gmail.com>
2026-01-10 19:18:32 +0530

    fixing cors for backend

--- index.ts
+++ index.ts
@@ -13,27 +13,29 @@ import razorpayRoutes from "./routes/payment";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:8080",
-  "https://zentik-fashion.vercel.app/",
-  "https://zercindia.in",
-];
-
 app.use(
   cors({
     origin: (origin, callback) => {
-      
       if (!origin) return callback(null, true);
+
+      const allowedOrigins = [
+        "http://localhost:8080",
+        "https://zentik-fashion.vercel.app",
+        "https://zercindia.in",
+      ];
 
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
 
-      return callback(new Error("Not allowed by CORS"));
+      return callback(new Error(`CORS blocked origin: ${origin}`));
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
