85abed19eb88018982f56bdd431cc9c513cbc648
harshitrwt <harshit.rawat1804@gmail.com>
2026-01-11 15:49:20 +0530

    fixing bcrypt and login issue

--- utils/bcrypt.ts
+++ utils/bcrypt.ts
@@ -1,5 +1,10 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
-export const hashPassword = (p: string) => bcrypt.hash(p, 12);
-export const verifyPassword = (p: string, h: string) =>
-  bcrypt.compare(p, h);
+export const hashPassword = async (p: string) => {
+  return await bcrypt.hash(p, 12);
+};
+
+export const verifyPassword = async (p: string, h: string) => {
+  return await bcrypt.compare(p, h);
+};
+
